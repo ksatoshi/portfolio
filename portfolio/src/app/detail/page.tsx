400fff0b9de70c0159acb6e044381f67b3bebac2
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2024-04-18 15:01:15 +0900

    init next

---
+++ portfolio/src/app/detail/page.tsx
@@ -0,0 +1,70 @@
+export default function Detail() {
+    return(
+        <>
+            <h1>経歴</h1>
+            <table>
+                <tr>
+                    <td>2017年</td>
+                    <td>函館工業高等専門学校 生産システム工学科 入学</td>
+                </tr>
+                <tr>
+                    <td>2022年</td>
+                    <td>函館工業高等専門学校 生産システム工学科情報コース 卒業</td>
+                </tr>
+                <tr>
+                    <td/>
+                    <td>公立はこだて未来大学システム情報科学部複雑系知能学科知能システムコース 編入</td>
+                </tr>
+                <tr>
+                    <td>2025年</td>
+                    <td>公立はこだて未来大学システム情報科学部複雑系知能学科知能システムコース 卒業見込み</td>
+                </tr>
+            </table>
+            <h1>活動履歴</h1>
+            <table>
+                <tr>
+                    <td>2020年</td>
+                    <td>高専プロコン</td>
+                    <td>教育支援ソフト(発案のみ,未実装)</td>
+                    <td>予選落ち</td>
+                    <td/>
+                </tr>
+                <tr>
+                    <td/>
+                    <td>DCON</td>
+                    <td>わかめの飼育状況監視サービス(発案のみ,未実装)</td>
+                    <td>予選落ち</td>
+                    <td/>
+                </tr>
+                <tr>
+                    <td>2021年</td>
+                    <td>HackU Kosen2021</td>
+                    <td>災害時に避難経路を自動で通知するLINE Bot</td>
+                    <td/>
+                    <td><a href="https://github.com/hashiba-k-jp/hakodate-a05">Gitリポジトリ</a></td>
+                </tr>
+                <tr>
+                    <td>2023年</td>
+                    <td>Hack With Hakdoate</td>
+                    <td>マルチリンガルかつ画像認識を用いることによってゴミ捨てを支援するサービス</td>
+                    <td>受賞</td>
+                    <td><a href="https://github.com/rgga-16/Hakodate-Hackathon">Gitリポジトリ</a></td>
+                </tr>
+                <tr>
+                    <td/>
+                    <td>p2hacks</td>
+                    <td>スタッフ兼メンターとして参加</td>
+                    <td/>
+                    <td/>
+                </tr>
+                <tr>
+                    <td>2024</td>
+                    <td>PR TIMES HACKATHON 2024 Winter</td>
+                    <td>プレスリリースを時系列で地図にマッピングするサービス</td>
+                    <td>チーム賞受賞</td>
+                    <td/>
+                </tr>
+            </table>
+        </>
+    )
+}
