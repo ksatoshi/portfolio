57fe22d70c44e1b978cf718f981a0c841d659846
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2024-06-21 19:44:06 +0900

    Topページの改修

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,12 @@ export default function RootLayout({
     <html lang="ja">
       <Analytics/>
       <SpeedInsights />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+        
+      </body>
     </html>
   );
 } 
